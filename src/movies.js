3f6540e3ca5ed7921fecbce9206c3585d1b1e780
bishopwm <bishopwm@g.cofc.edu>
2020-04-02 15:36:47 -0400

    iteration 3

--- src/movies.js
+++ src/movies.js
@@ -1,44 +1,72 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-
-
+// ITERATION 1: Ordering by year - Order by year, ascending (in growing order)
 
 function compare(movieA,movieB) {
-    const firstMovie = movieA.year;
-    const secondMovie = movieB.year;
+    const earlierMovie = movieA.year;
+    const laterMovie = movieB.year;
 
     let comparison = 0;
-    if (firstMovie > secondMovie) {
+    if (earlierMovie > laterMovie) {
         comparison = 1;
-    } else if (firstMovie < secondMovie) {
+    } else if (earlierMovie < laterMovie) {
         comparison = -1;
-    }
-    //console.log(comparison)
+    } 
     return comparison
 }
 
 let sortedArray = movies.sort(compare);
 
 function orderByYear(arr){
-    return sortedArray;
+  let finalSortedArr = [];
+  for (i=0; i<arr.length; i++){
+    let newMovieObj = {year: 0, title: "some title"};
+    newMovieObj.year = sortedArray[i].year;
+    newMovieObj.title = sortedArray[i].title;
+    
+    finalSortedArr.push(newMovieObj);
+
+  }
+  return finalSortedArr;
 }
 
 orderByYear(movies);
 
-// function returnSingleElement(arr){
-//     for(i=0; i<arr.length; i++){
-//         let singleElements = [];
-//         singleElements.push(arr[2])
-//         return singleElements
-//     }
-// }
+//let sortedArrayVar = orderByYear(movies);
 
-// returnSingleElement(sortedArray);
+// ITERATION 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(arr){
+    let totalCount = 0
+    for(i=0; i<arr.length; i++){
+      let bothConditions = movies[i].genre.includes("Drama") && movies[i].director.includes("Steven Spielberg");
+      if(bothConditions == true) {
+       totalCount++
+     };
+    }
+    return totalCount
+  }
+  
+  howManyMovies(movies);
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+let firstTwenty = [];
+
+const orderAlphabetically = (arr) => {
+    if(arr.length>=20){
+        for(i=0; i<20; i++){
+            firstTwenty.push(arr[i])
+        }
+    } else if (arr.length<20){
+        for(i=0; i<arr.length; i++){
+            firstTwenty.push(arr[i])
+        }
+    }
+    return firstTwenty
+}
+
+orderAlphabetically(movies);
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
