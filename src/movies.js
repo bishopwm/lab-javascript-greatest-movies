2c27e1e88438cac5d2c2603f005b454c147636cc
bishopwm <bishopwm@g.cofc.edu>
2020-04-02 10:42:36 -0400

    order movies by year -- started

--- src/movies.js
+++ src/movies.js
@@ -2,6 +2,40 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
+
+function compare(movieA,movieB) {
+    const firstMovie = movieA.year;
+    const secondMovie = movieB.year;
+
+    let comparison = 0;
+    if (firstMovie > secondMovie) {
+        comparison = 1;
+    } else if (firstMovie < secondMovie) {
+        comparison = -1;
+    }
+    //console.log(comparison)
+    return comparison
+}
+
+let sortedArray = movies.sort(compare);
+
+function orderByYear(arr){
+    return sortedArray;
+}
+
+orderByYear(movies);
+
+// function returnSingleElement(arr){
+//     for(i=0; i<arr.length; i++){
+//         let singleElements = [];
+//         singleElements.push(arr[2])
+//         return singleElements
+//     }
+// }
+
+// returnSingleElement(sortedArray);
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
